26b8ede5cbedb5b1f396ea45ff7c5b09629f35eb
Justin Koch <jckoch@contractor.usgs.gov>
2025-04-10 08:38:11 -0500

    [LPDA-4980] Added unit tests for the 'plugin' and 'util' modules; improved readability of unit testing shell script.

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -6,4 +6,7 @@ tgt_python="${1:-/Applications/QGIS.app/Contents/MacOS/bin/python3.9}"
 echo "using QGIS Python at ${tgt_python}"
 
 # run all unit tests with coverage report
-$tgt_python -m coverage run -m pytest -v plugin/test/* --tb=long --disable-pytest-warnings && $tgt_python -m coverage report -m && $tgt_python -m coverage html -d cover
+$tgt_python -m coverage run -m pytest -v plugin/test/* \
+    --tb=long --disable-pytest-warnings && \
+  $tgt_python -m coverage report -m && \
+  $tgt_python -m coverage html -d cover
