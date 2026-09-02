77117484f93d3b5f743132718efe71f03d98f20d
Erik Bolch <ebolch@contractor.usgs.gov>
2026-09-02 13:49:16 -0500

    Fix run_unit_tests.sh to work on Windows

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -6,7 +6,7 @@ tgt_python="${1:-/Applications/QGIS.app/Contents/MacOS/bin/python3.9}"
 echo "using QGIS Python at ${tgt_python}"
 
 # run all unit tests with coverage report
-$tgt_python -m coverage run -m pytest -v plugin/test/* \
+"$tgt_python" -m coverage run -m pytest -v --pyargs plugin.test \
     --tb=long --disable-pytest-warnings && \
-  $tgt_python -m coverage report -m && \
-  $tgt_python -m coverage html -d cover
+  "$tgt_python" -m coverage report -m && \
+  "$tgt_python" -m coverage html -d cover
